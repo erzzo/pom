8ed9f9865bdf6e1d3bdaf05dd02d090d84eea60f
erzzo <pohmerik@gmail.com>
2013-11-13 15:33:52 +0100

    Add subject

--- app/models/Subject.php
+++ app/models/Subject.php
@@ -3,5 +3,18 @@ namespace Models;
 
 class Subject extends Base
 {
+	public function getSchoolYears()
+	{
+		return $this->db->table('school_year');
+	}
 
+	public function addEdit($values, $id = NULL)
+	{
+		if(is_null($id)) {
+			return $this->getTable()->insert($values);
+		} else {
+			$subject = $this->getTable()->get($id);
+			return $subject->update($values);
+		}
+	}
 }

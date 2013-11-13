1a7cbf2ae248a72253d3c98b35c1a75cd1bf9a12
erzzo <pohmerik@gmail.com>
2013-11-13 15:15:05 +0100

    Add subject not complete

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,9 +6,11 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 	protected $projectModel;
+	protected $subjectModel;
 
-	public function injectBase(Models\Subject $project)
+	public function injectBase(Models\Project $project, Models\Subject $subject)
 	{
 		$this->projectModel = $project;
+		$this->subjectModel = $subject;
 	}
 }

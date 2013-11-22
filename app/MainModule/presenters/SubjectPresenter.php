0c4faf755819e96be4ae6f2967c05faaaff5bad3
erzzo <pohmerik@gmail.com>
2013-11-22 15:16:46 +0100

    vratene funkcie...

--- app/MainModule/presenters/SubjectPresenter.php
+++ app/MainModule/presenters/SubjectPresenter.php
@@ -21,51 +21,6 @@ class SubjectPresenter extends BasePresenter
 
 	}
 
-	public function actionAddEdit($id)
-	{
-		if ($id) {
-			$subject = $this->subjectModel->get($id);
-			if (!$subject) {
-				//throw Nette\
-			}
-			$this['addEditSubjectForm']->setDefaults($subject);
-		}
-	}
-
-	public function createComponentAddEditSubjectForm()
-	{
-		$schoolYear = $this->subjectModel->getSchoolYears()->fetchPairs('id', 'year');
-		$terms = $this->subjectModel->getTerms()->fetchPairs('id', 'name');
-		$grades = $this->subjectModel->getGrades()->fetchPairs('id', 'grade');
-
-		$form = new Form;
-		$form->addSelect('school_year_id', 'Skolský rok', $schoolYear)
-			->setRequired('Povinný atribút');
-		$form->addSelect('term_id', 'Semester', $terms)
-			->setRequired('Povinný atribút');
-		$form->addSelect('grade_id', 'Ročník', $grades)
-			->setRequired('Povinný atribút');
-		$form->addText('name', 'Názov predmetu')
-			->setRequired('Povinný atribút');
-		$form->addText('password', 'Heslo na vstup');
-		$form->addSubmit('submit');
-		$form->onSuccess[] = $this->processAddEditSubjectForm;
-
-		return $form;
-	}
-
-	public function processAddEditSubjectForm(Form $form)
-	{
-		$values = $form->getValues();
-		$values->created = new \Nette\Datetime;
-		$id = $this->presenter->getParameter('id');
-
-		$this->subjectModel->addEdit($values, $id);
-		$this->flashMessage("Predmet bol pridaný");
-
-		$this->redirect('showMy');
-	}
-
 	public function createComponentFilterSubjectsForm()
 	{
 		$grades = $this->subjectModel->getGrades()->fetchPairs('id','grade');

f9f3bc26f522d543f4fd5666c167cf475c384fbb
Visvk <viktor.sincak@gmail.com>
2013-11-24 22:35:46 +0100

    fileUpload refactor. Download file. Show files

--- app/MainModule/presenters/TaskPresenter.php
+++ app/MainModule/presenters/TaskPresenter.php
@@ -34,6 +34,31 @@ class TaskPresenter extends BasePresenter
 		}
 	}
 
+	public function handleDownloadFile($taskId) {
+		$fileDat= $this->fileModel->get($taskId);
+
+		$file = $this->context->params['wwwDir'].'/../storage/files/'. $fileDat->url;
+		$fileName = $fileDat->name;
+		$httpResponse = $this->context->getService('httpResponse');
+		$httpResponse->setHeader('Pragma', "public");
+		$httpResponse->setHeader('Expires', 0);
+		$httpResponse->setHeader('Cache-Control', "must-revalidate, post-check=0, pre-check=0");
+		$httpResponse->setHeader('Content-Transfer-Encoding', "binary");
+		$httpResponse->setHeader('Content-Description', "File Transfer");
+		if( $httpResponse->setHeader('Content-Length', filesize($file)) &&
+			$this->sendResponse(new FileResponse($file, $fileName,'application/octet-stream,application/force-download, application/download'))
+		){$this->flashMessage('Súbor stiahnutý.', 'success');
+		}else{
+			$this->flashMessage('Problém pri sťahovaní súboru.', 'error');
+		}
+		if (!$this->presenter->isAjax()) {
+
+			$this->presenter->redirect('this');
+		}
+
+		$this->invalidateControl();
+	}
+
 	public function createComponentAddEditTaskForm()
 	{
 		$users = $this->themeModel->getThemeUsers($this->presenter->params['themeId']);
@@ -93,16 +118,23 @@ class TaskPresenter extends BasePresenter
 		$fileCount = $this->fileModel->getAll()->where('task_id', $taskId)->count();
 		if ($task && $task->max_files_count > $fileCount) {
 			if($values['file']->isOk()){
-				//$values['extension'] = pathinfo($values['file']->getName(), PATHINFO_EXTENSION); //TODO: treba do DB dat extension
-				//$values['file_name'] = $values['name']->getName().new DateTime().'.'.$values['extension'];
-
-				$values['url'] = $URL = Strings::webalize('subject'. $task->theme->project->subject->name .'/project'.$task->theme->project->name .'/theme'.$task->theme->name.'/task'. $task->name, NULL, FALSE);
-				if (!file_exists($this->context->params['wwwDir'] . '/../storage/files/'.$URL)) {
-					//mkdir($this->context->params['wwwDir'] . '/../storage/files/subject'.$URL, 0755, true);
+				$values['extension'] = pathinfo($values['file']->getName(), PATHINFO_EXTENSION); //TODO: treba do DB dat extension
+				$values['file_name'] = $values['name'].new DateTime().'.'.$values['extension']; //TODO: datum na Y-m-d
+				$urlSubject = Strings::webalize('subject'. $task->theme->project->subject->name, NULL, FALSE);
+				$urlProject = Strings::webalize('project'.$task->theme->project->name, NULL, FALSE);
+				$urlTheme = Strings::webalize('theme'.$task->theme->name, NULL, FALSE);
+				$urlTask = Strings::webalize('task'. $task->name, NULL, FALSE);
+				$URL = $urlSubject.'/'.$urlProject.'/'.$urlTheme.'/'.$urlTask;
+				if (!file_exists($this->context->params['wwwDir'] . '/storage/files/'.$URL)) {
+					mkdir($this->context->params['wwwDir'] . '/../storage/files/'.$URL, 0755, true);
 				}
-				//$values['name']->move($this->context->params['wwwDir'] . '/../storage/files/'.$URL);
+				$values['url'] = $URL.'/'.$values['file_name'];
+				//$values['file']->move($this->context->params['wwwDir'] . '/../storage/files/'.$values['url']); //TODO: Daco tu nejde, mozno na hrone by to slo :D
+
 				//DB ukladanie
 				unset($values['file']);
+				unset($values['extension']);
+				unset($values['file_name']);
 				$values['theme_id'] = $task->theme_id;
 				$values['task_id'] = $task->id;
 				$values['created'] = new \Nette\DateTime;

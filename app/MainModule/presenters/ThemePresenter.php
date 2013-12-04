194de1e74120c1c569e55aa82813158cb44e693d
erzzo <pohmerik@gmail.com>
2013-12-05 00:43:39 +0100

    charts: data

--- app/MainModule/presenters/ThemePresenter.php
+++ app/MainModule/presenters/ThemePresenter.php
@@ -10,6 +10,22 @@ class ThemePresenter extends BasePresenter
 	{
 		$this->template->subject = $this->subjectModel->get($subjectId);
 		$this->template->themes = $this->themeModel->getMyThemes($subjectId);
-		$this->template->taskPercentage = $this->themeModel->getThemePercentage($subjectId);
+		$this->template->taskPercentage = $taskPercentage = $this->themeModel->getThemePercentage($subjectId);
+
+		$flotPercentage = array();
+		foreach ($taskPercentage as $key => $perc) {
+			$flotPercentage[$key][0] = array(
+				"label" => "Splnene",
+				"data" => $perc,
+				"color" => "#00D50F"
+			);
+			$flotPercentage[$key][1] = array(
+				"label" => "Nesplnene",
+				"data" => 100-$perc,
+				"color" => "#FA0000"
+			);
+		}
+
+		$this->template->flotPercentage = json_encode($flotPercentage);
 	}
 }
